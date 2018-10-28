730f5f4fbeac49b5fdd7ecc28a8b9128ce152037
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-28 07:28:14 -0400

    Start Repo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span>
+        {{mensaje}}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      // Cambiar a mayusculas usando un filtro
+      mensaje: 'Aprende Vue Js 2 Facilmente'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
