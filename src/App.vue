6081c53984746dab33094e85c342e1411177ce58
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-28 07:34:15 -0400

    Creacion de un Filtro Local

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <div class="jumbotron">
       <span>
+        {{mensaje | mayusculas}}
+      </span>
+      <br>
+      <span>
         {{mensaje}}
       </span>
     </div>
@@ -14,6 +18,12 @@ export default {
     return {
       // Cambiar a mayusculas usando un filtro
       mensaje: 'Aprende Vue Js 2 Facilmente'
+    }
+  },
+  filters: {
+    // Debemos reibir el mensaje " mensaje | <= parte izq de la barra  "
+    mayusculas(mensaje) {
+      return mensaje.toUpperCase();
     }
   }
 }
